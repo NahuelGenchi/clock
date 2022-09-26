adaaf764823124784783b7ead545b74ef31b4f3e
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-09-25 22:35:57 -0300

    adding timezone to Time

--- src/components/Time/Time.jsx
+++ src/components/Time/Time.jsx
@@ -9,12 +9,16 @@ const Time = () => {
       const hours = currentDate.getHours();
       const minutes = currentDate.getMinutes();
       const seconds = currentDate.getSeconds();
-      setTime({hours, minutes, seconds});
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      setTime({hours, minutes, seconds, timezone});
     }, 1000
   );
 
   return(
-    <p>{time?.hours} : {time?.minutes} : {time?.seconds < 10 ? `0${time?.seconds}` : time?.seconds}</p>
+    <>
+      <p className="tc-timezone">{time?.timezone}</p>
+      <p>{time?.hours} : {time?.minutes} : {time?.seconds < 10 ? `0${time?.seconds}` : time?.seconds}</p>
+    </>
   );
 };
 
