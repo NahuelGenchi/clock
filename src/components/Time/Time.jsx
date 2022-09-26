403345bea62e22c17ea99fd243d47036331eee17
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-09-25 22:18:48 -0300

    adding Time logic

--- src/components/Time/Time.jsx
+++ src/components/Time/Time.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 const Time = () => {
+  const [time, setTime] = useState({});
+
+  setInterval(
+    function(){
+      const currentDate = new Date();
+      const hours = currentDate.getHours();
+      const minutes = currentDate.getMinutes();
+      const seconds = currentDate.getSeconds();
+      setTime({hours, minutes, seconds});
+    }, 1000
+  );
+
   return(
-    <></>
+    <p>{time?.hours} : {time?.minutes} : {time?.seconds < 10 ? `0${time?.seconds}` : time?.seconds}</p>
   );
 };
 
